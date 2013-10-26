fee14e6e3a4e01f1f8a9ef9f97a46a8e3ec67226
aeife <andre.eife@gmail.com>
2013-10-26 22:46:34 +0200

    added category mock data

--- server/src/dbMock/dbMock.js
+++ server/src/dbMock/dbMock.js
@@ -3,20 +3,33 @@ db = conn.getDB("meanshop");
 
 // remove old db entries
 db.products.remove();
+db.categories.remove();
 
 load('Faker.js');
 
 var productCount = 10;
 
-var categories = [
+var categoryTitles = [
   'Notebook',
   'Tablets',
   'Smartphones',
   'Desktop',
   'Misc'
 ];
+var categories = [];
 
-// insert users
+// create categories
+for (var i = 0, len = categoryTitles.length; i < len; i++){
+  categories.push(ObjectId());
+  db.categories.insert({
+    _id: categories[i],
+    title: categoryTitles[i],
+    description: Faker.Lorem.sentences(3),
+    created: new Date()
+  });
+}
+
+// create products
 for (var i = 0; i < productCount; i++){
   db.products.insert({
     title: Faker.Lorem.words(1)[0],
