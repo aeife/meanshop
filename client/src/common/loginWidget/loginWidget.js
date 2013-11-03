9128377ccc6e5a48446689f5df5a116f97880214
aeife <andre.eife@gmail.com>
2013-11-04 00:27:14 +0100

    added login widget

---
+++ client/src/common/loginWidget/loginWidget.js
@@ -0,0 +1,15 @@
+angular.module('meanShop.loginWidget', [
+
+])
+
+.directive('loginWidget', function() {
+  return {
+    restrict: 'E',
+    templateUrl: 'loginWidget/loginWidget.tpl.html',
+    controller: function($scope){
+
+    }
+  };
+})
+
+;
---
+++ client/src/common/loginWidget/loginWidget.spec.js
@@ -0,0 +1,8 @@
+describe('login widget', function() {
+  beforeEach( module( 'meanShop.loginWidget' ) );
+
+  it('should have a dummy test', inject( function() {
+    expect(true).toBeTruthy();
+  }));
+});
+
