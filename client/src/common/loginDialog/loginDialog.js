b067ee5f19d4c2aa23aecc6480145e886218f583
aeife <andre.eife@gmail.com>
2013-11-05 03:31:10 +0100

    adjusted login modal

--- client/src/common/loginDialog/loginDialog.js
+++ client/src/common/loginDialog/loginDialog.js
@@ -3,7 +3,27 @@ angular.module('meanShop.loginDialog', [
 ])
 
 .controller('LoginDialogCtrl', function ($scope, auth, $modalInstance) {
+  $scope.signInData = {
+    username: '',
+    password: ''
+  };
+  $scope.registerData = {
+    username: '',
+    password: '',
+    passwordConfirm: ''
+  };
 
+  $scope.login = function(){
+    console.log($scope.signInData);
+  };
+
+  $scope.register = function(){
+    console.log($scope.registerData);
+  };
+
+  $scope.close = function(){
+    $modalInstance.close();
+  };
 })
 
 ;
---
+++ client/src/common/loginDialog/loginDialog.spec.js
@@ -0,0 +1,7 @@
+describe('login modal', function() {
+  beforeEach(module('meanShop.loginDialog'));
+
+  it('should have a dummy test', inject( function() {
+    expect(true).toBeTruthy();
+  }));
+});
