08e3a413129b3e2826f3bfcb40bcc73a557f042d
aeife <andre.eife@gmail.com>
2013-11-06 20:20:55 +0100

    fixed module for test

--- client/src/app/shop/categoryChooser/categoryChooser.spec.js
+++ client/src/app/shop/categoryChooser/categoryChooser.spec.js
@@ -1,5 +1,5 @@
 describe( 'category chooser section', function() {
-  beforeEach(module('meanShop.shop.shopCategory'));
+  beforeEach(module('meanShop.shop.categoryChooser'));
 
   it('should have a dummy test', inject( function() {
     expect(true).toBeTruthy();
